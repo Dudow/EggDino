48fc8e1310427a2c882b9380a6d8dfb2ba093c9a
Eduardo Nunes <edu30.nm@gmail.com>
2020-11-03 01:07:32 +0000

    inicio da modularização de comandos

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
-
+const Discord = require("discord.js")
+const client = new Discord.Client()
 const moment = require('moment'); 
 
+const command = require('./commands/commands.js')
+
+const config = require("./config.json")
 app.get("/", (req, res) => {
   const ping = new Date()
   ping.setHours(ping.getHours() - 3)
@@ -11,64 +15,42 @@ app.get("/", (req, res) => {
 })
 app.listen(process.env.PORT)
 
-const Discord = require("discord.js")
-const client = new Discord.Client()
-const config = require("./config.json")
 
-client.on("message", msg => {
-  if(msg.author.bot) return
-  // if(msg.channel.type == "dm") return
-  if(!msg.content.toLowerCase().startsWith(config.prefix)) return
-  if(msg.content.startsWith(`<@!${client.user.id}>`) ||
-  msg.content.startsWith(`<@${client.user.id}>`)) return
+client.on('ready', () => {
+  command(client, 'ping', (msg) => {
+    msg.channel.send('pong')
+  })
 
-  const args = msg.content.trim().slice(config.prefix.lenght).split(/ +/g)
-  const command = args.shift().toLowerCase()
+  command(client, 'ajuda', (msg) => {
+    msg.channel.send(`Fala comédia, por enquanto você tem os comandos:
+        //horas para conferir o horário atual em DIVERSOS países
+        //ajuda pra voltar pra cá
+        //addme pra me colocar no seu server`)
+  })
 
-  try{
-    const commandFile = require(`./commands/${command}.js`)
-    commandFile.run(client, msg, args)
-  } catch(err){
-    console.log("Erro: " + err)
-  }
-})
+  command(client, ['hours', 'horas'], (msg) => {
+    let indiaHours = moment(new Date()).add(5, 'hours').add(30, 'minutes').format("HH:mm")
+    let brazilHours = moment(new Date()).subtract(3, 'hours').format("HH:mm")
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
+    msg.channel.send(`Brazil: ${brazilHours} `)
+    msg.channel.send(`India: ${indiaHours}`)
+  })
 
-client.on('message', msg => {
-  if (msg.content === '//help') {
-    msg.reply(`
+  command(client, 'help', (msg) => {
+    msg.channel.send(`
       Talk, Pagliacci, for a while you have this commands:
         //hours to see the current hours in all the countries
         //help to come back to here
         //addme to invite me to your server
-    `);
-  }
-});
+    `)
+  })
 
-client.on('message', msg => {
-  if (msg.content === '//ajuda') {
-    msg.reply(`
-      Fala comédia, por enquanto você tem os comandos:
-        //horas para conferir o horário atual em DIVERSOS países
-        //ajuda pra voltar pra cá
-        //addme pra me colocar no seu server
-    `);
-  }
-});
-
-client.on('message', msg => {
-  if (msg.content === '//addme') {
-
-    msg.reply(`
+  command(client, 'addme', (msg) => {
+    msg.channel.send(`
       Use this link: https://discord.com/oauth2/authorize?client_id=772102488005935114&scope=bot&permissions=3145
-    `);
-  }
-});
+    `)
+  })
+})
 
 client.on('message', msg => {
 
@@ -76,7 +58,7 @@ client.on('message', msg => {
 
   if(msg.content.match(regex))
   {
-    msg.reply(`O SEU FILHO DA PUTA, SE MATA AGORA`);
+    msg.reply(`DELETE THIS`);
   }
 });
 
